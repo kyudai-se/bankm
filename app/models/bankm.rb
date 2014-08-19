4fe894c2eac3865ea8ba2a7034b11dd33cf3920e
kyudai <nishio@kyudai.co.jp>
2014-08-19 16:34:30 +0900

    bank_cd

--- app/models/bankm.rb
+++ app/models/bankm.rb
@@ -7,8 +7,8 @@ class Bankm < ActiveRecord::Base
   validates :entclt,  length: { maximum: 20 }
   validates :edtmcn,  length: { maximum: 20 }
   validates :edtclt,  length: { maximum: 20 }
-  validates :bank_cd, presence: true, length: { maximum: 04 }, uniqueness: true,
-            numericality: true
+  validates :bank_cd, presence: true, uniqueness: true,
+            numericality: { only_integer: true, less_than: 10000 }
   validates :bank_j,  presence: true, length: { maximum: 40 }, uniqueness: true,
             format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}A-Za-zＡ-Ｚ-ー]+\z/, allow_blank: true, message: 'はアルファベット、漢字、ひらがな、カタカナのいずれかで入力してください' }
   validates :bank_a,  presence: true, length: { maximum: 40 }, uniqueness: true,
