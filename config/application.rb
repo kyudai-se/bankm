d79646b11fdb4833b38a95c3585b34cc091b0efc
kyudai <nishio@kyudai.co.jp>
2014-08-19 14:35:27 +0900

    TimeZone

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,11 @@ module Bank
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     #
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
