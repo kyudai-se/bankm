0fce1e8bd538aaabeb0fb1313600a1df8d708e6d
kyudai <nishio@kyudai.co.jp>
2014-08-22 08:54:11 +0900

    default

--- db/migrate/20140821234327_bank_cd.rb
+++ db/migrate/20140821234327_bank_cd.rb
@@ -1,4 +1,16 @@
 class BankCd < ActiveRecord::Migration
   def change
+    create_table "bankm", force: true do |t|
+      t.string   "entmcn",  limit: 20, default: "",  null: false
+      t.string   "entclt",  limit: 20, default: "",  null: false
+      t.string   "edtmcn",  limit: 20, default: "",  null: false
+      t.string   "edtclt",  limit: 20, default: "",  null: false
+      t.datetime "entdate"
+      t.datetime "edtdate"
+      t.string   "bank_j",  limit: 40, default: "",  null: false
+      t.string   "bank_a",  limit: 40, default: "",  null: false
+      t.string   "bank_k",  limit: 40, default: "",  null: false
+      t.decimal  "bank_cd", precision: 4, scale: 0,  default: 0, null: false
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821232621) do
+ActiveRecord::Schema.define(version: 20140821234327) do
 
   create_table "aream", force: true do |t|
     t.string   "entmcn",  limit: 20,                         default: "", null: false
@@ -27,16 +27,16 @@ ActiveRecord::Schema.define(version: 20140821232621) do
   end
 
   create_table "bankm", force: true do |t|
-    t.string   "entmcn",  limit: 20, default: "",  null: false
-    t.string   "entclt",  limit: 20, default: "",  null: false
-    t.string   "edtmcn",  limit: 20, default: "",  null: false
-    t.string   "edtclt",  limit: 20, default: "",  null: false
+    t.string   "entmcn",  limit: 20,                         default: "", null: false
+    t.string   "entclt",  limit: 20,                         default: "", null: false
+    t.string   "edtmcn",  limit: 20,                         default: "", null: false
+    t.string   "edtclt",  limit: 20,                         default: "", null: false
     t.datetime "entdate"
     t.datetime "edtdate"
-    t.string   "bank_j",  limit: 40, default: "",  null: false
-    t.string   "bank_a",  limit: 40, default: "",  null: false
-    t.string   "bank_k",  limit: 40, default: "",  null: false
-    t.decimal  "bank_cd", precision: 4, scale: 0,  default: 0.0, null: false
+    t.string   "bank_j",  limit: 40,                         default: "", null: false
+    t.string   "bank_a",  limit: 40,                         default: "", null: false
+    t.string   "bank_k",  limit: 40,                         default: "", null: false
+    t.decimal  "bank_cd",            precision: 4, scale: 0, default: 0,  null: false
   end
 
 end
